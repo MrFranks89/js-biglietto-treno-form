ccfaba67aefe3126df663c6cb341691921253d85
MrFranks89 <federico.franchi89@gmail.com>
2024-07-04 15:03:17 +0200

    Add: Form structure, js functions. No results

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log('calcola biglietto')
+
+const formElement = document.getElementById('form-biglietto')
+console.log(formElement)
+
+const inputKmElement = document.getElementById('km')
+const selectAgeElement = document.getElementById('age')
+const ticketOutputElement = document.getElementById('ticket-output')
+console.log(
+	inputKmElement,
+    selectAgeElement,
+	ticketOutputElement
+)
+
+formElement.addEventListener('submit', function (event) {
+
+	event.preventDefault()
+    const km = parseFloat(inputKmElement.value)
+    const prezzoBase = parseFloat(inputKmElement.value * 0.21)
+    const selectedAge = selectAgeElement.value
+    console.log(selectedAge, prezzoBase.toFixed(2))
+
+    let sconto = 0
+
+    if (selectedAge === 'u18'){
+        sconto = prezzoBase * 0.2
+    }
+    else if (selectedAge === 'o65'){
+        sconto = prezzoBase * 0.4
+    }
+        
+    console.log(sconto)
+
+    let prezzoFinale = prezzoBase - sconto
+    console.log(prezzoFinale.toFixed(2))
+
+    // ticketOutputElement.innerHTML = prezzoFinale + '€'
+})
+
